b0222a5b3eca231cce9e004567ecf7baf0ae04a8
nzmpi <nizami.abdulov@gmail.com>
2025-08-08 15:39:52 +0400

    test: add tests

---
+++ test/utils/Implementations.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Implementation {
+    bool flag;
+
+    function toggle() external {
+        flag = !flag;
+    }
+
+    function getFlag() external view returns (bool) {
+        return flag;
+    }
+}
+
+contract Implementation2 is Implementation {
+    function initialize(bool _flag) external {
+        flag = _flag;
+    }
+
+    function getReverseFlag() external view returns (bool) {
+        return !flag;
+    }
+}
